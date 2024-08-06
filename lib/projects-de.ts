7e843e15fa4afbc3d022cc1166bf4574d277f5d8
notkoyo <kaiden01@outlook.de>
2024-08-06 20:42:30 +0100

    update project ordering

--- lib/projects-de.ts
+++ lib/projects-de.ts
@@ -1,19 +1,5 @@
 export default [
   {
-    projectTitle: "KokoBot",
-    projectHref: "https://github.com/notkoyo/koko-bot",
-    projectDescription:
-      "Entwicklung eines Valorant-Custom-Games-Discord-Bots zur Erstellung und Verwaltung von benutzerdefinierten Spiel-Lobbies. Entwickelt mit JavaScript, discord.js und MongoDB.",
-    projectImage: "/koko-bot.png",
-  },
-  {
-    projectTitle: "Jobi CLI",
-    projectHref: "https://github.com/notkoyo/jobi-cli",
-    projectDescription:
-      "Entwicklung von Jobi, einem Command Line Interface (CLI) Tool zur effizienten Verwaltung von Bewerbungen. Mit diesem Tool können alle in einer MongoDB-Datenbank gespeicherten Bewerbungen hinzugefügt, aktualisiert, gelöscht und angezeigt werden.",
-    projectImage: "/jobi-cli.png",
-  },
-  {
     projectTitle: "NC News",
     projectHref: "https://github.com/notkoyo/ncnews",
     projectDescription:
@@ -33,5 +19,19 @@ export default [
     projectDescription:
       "Entwicklung einer benutzerorientierten Plattform zur Verwaltung und Suche von Veranstaltungen, erstellt mit Next.js und Supabase.",
     projectImage: "/congregate.png",
+  },
+  {
+    projectTitle: "KokoBot",
+    projectHref: "https://github.com/notkoyo/koko-bot",
+    projectDescription:
+      "Entwicklung eines Valorant-Custom-Games-Discord-Bots zur Erstellung und Verwaltung von benutzerdefinierten Spiel-Lobbies. Entwickelt mit JavaScript, discord.js und MongoDB.",
+    projectImage: "/koko-bot.png",
+  },
+  {
+    projectTitle: "Jobi CLI",
+    projectHref: "https://github.com/notkoyo/jobi-cli",
+    projectDescription:
+      "Entwicklung von Jobi, einem Command Line Interface (CLI) Tool zur effizienten Verwaltung von Bewerbungen. Mit diesem Tool können alle in einer MongoDB-Datenbank gespeicherten Bewerbungen hinzugefügt, aktualisiert, gelöscht und angezeigt werden.",
+    projectImage: "/jobi-cli.png",
   },
 ] as const;
--- lib/projects.ts
+++ lib/projects.ts
@@ -1,17 +1,5 @@
 export default [
   {
-    projectTitle: "KokoBot",
-    projectHref: "https://github.com/notkoyo/koko-bot",
-    projectDescription: "A Valorant custom games discord bot for creating and managing custom game lobbies. Built with JavaScript, discord.js, and MongoDB.",
-    projectImage: "/koko-bot.png"
-  },
-  {
-    projectTitle: "Jobi CLI",
-    projectHref: "https://github.com/notkoyo/jobi-cli",
-    projectDescription: "Jobi is a Command Line Interface (CLI) tool designed to help you manage your job applications efficiently. With this tool, you can add, update, delete, and view all job applications stored in a MongoDB database.",
-    projectImage: "/jobi-cli.png"
-  },
-  {
     projectTitle: "NC News",
     projectHref: "https://github.com/notkoyo/ncnews",
     projectDescription: "Front-end project to visualise the data served by the NC News API. Built with Next.js, Tailwind CSS, and Vercel.",
@@ -28,5 +16,17 @@ export default [
     projectHref: "https://github.com/notkoyo/congregate",
     projectDescription: "A user-focused event management and finding platform built with Next.js and Supabase.",
     projectImage: "/congregate.png"
-  }
+  },
+  {
+    projectTitle: "KokoBot",
+    projectHref: "https://github.com/notkoyo/koko-bot",
+    projectDescription: "A Valorant custom games discord bot for creating and managing custom game lobbies. Built with JavaScript, discord.js, and MongoDB.",
+    projectImage: "/koko-bot.png"
+  },
+  {
+    projectTitle: "Jobi CLI",
+    projectHref: "https://github.com/notkoyo/jobi-cli",
+    projectDescription: "Jobi is a Command Line Interface (CLI) tool designed to help you manage your job applications efficiently. With this tool, you can add, update, delete, and view all job applications stored in a MongoDB database.",
+    projectImage: "/jobi-cli.png"
+  },
 ] as const;
